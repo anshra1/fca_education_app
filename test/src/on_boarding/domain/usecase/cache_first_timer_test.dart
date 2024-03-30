5dff6239d148e0fddcc2768a67e803a41b27c160
ansh raj <020ansh@gmail.com>
2024-03-30 17:12:41 +0530

    usecaseTest

---
+++ test/src/on_boarding/domain/usecase/cache_first_timer_test.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:fca_education_app/%20core/errors/failure.dart';
+import 'package:fca_education_app/src/on_boarding/domain/repo/on_boarding_repo.dart';
+import 'package:fca_education_app/src/on_boarding/domain/usecase/cache_first_timer.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockOnBoardingRepo extends Mock implements OnBoardingRepo {}
+
+void main() {
+  group('CacheFirstTimer', () {
+    late CacheFirstTimer cacheFirstTimer;
+    late OnBoardingRepo mockRepo;
+
+    setUp(() {
+      mockRepo = MockOnBoardingRepo();
+      cacheFirstTimer = CacheFirstTimer(repo: mockRepo);
+    });
+
+    var tServerFailure = ServerFailure(
+      message: 'message',
+      statusCode: 'statusCode',
+    );
+
+    test('should call cacheFirstTimer and return void', () async {
+      // Arrange
+      when(() => mockRepo.cacheFirstTimer()).thenAnswer(
+        (_) async => Left(tServerFailure),
+      );
+
+      // Act
+      final result = await cacheFirstTimer.call();
+
+      // Assert
+      expect(
+        result,
+        Left<ServerFailure, dynamic>(tServerFailure),
+      );
+      verify(() => mockRepo.cacheFirstTimer()).called(1);
+    });
+  });
+}
---
+++ test/src/on_boarding/domain/usecase/check_user_is_first_timer_test.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:fca_education_app/%20core/errors/failure.dart';
+import 'package:fca_education_app/src/on_boarding/domain/repo/on_boarding_repo.dart';
+import 'package:fca_education_app/src/on_boarding/domain/usecase/check_user_is_first_timer.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockOnBoardingRepo extends Mock implements OnBoardingRepo {}
+
+void main() {
+  group('checkIfUserIsFirstTimer', () {
+    late CheckUserIsFirstTimer firstTimer;
+    late OnBoardingRepo mockRepo;
+
+    setUp(() {
+      mockRepo = MockOnBoardingRepo();
+      firstTimer = CheckUserIsFirstTimer(repo: mockRepo);
+    });
+
+    test('should call checkIfUserIsFirstTimer and return true', () async {
+      // Arrange
+      when(() => mockRepo.checkIfUserIsFirstTimer()).thenAnswer(
+        (_) async => const Right(true),
+      );
+
+      // Act
+      final result = await firstTimer.call();
+
+      // Assert
+      expect(result, const Right<ServerFailure, bool>(true));
+      verify(() => mockRepo.checkIfUserIsFirstTimer()).called(1);
+      verifyNoMoreInteractions(mockRepo);
+    });
+  });
+}
